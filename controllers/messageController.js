ea8c309e9c1087c2f0104f8c65379d8f5e313e6b
mdtanvirteam <mdtanvir.ahmed@teamgrid.io>
2024-08-06 11:48:51 +0530

    done merge

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -49,50 +49,7 @@ class messageController {
  
   
   
-  // static addMessage = async (req, res) => {
-  //   try {
-  //     const { leagueId } = req.params;
-  //     const { userId, message } = req.body;
-
-  //     // Validate required fields
-  //     if (!leagueId || !userId || !message) {
-  //       return res.status(400).json({ error: "League ID, user ID, and message are required" });
-  //     }
-
-  //     // Find the league
-  //     const league = await League.findById(leagueId);
-  //     if (!league) {
-  //       return res.status(404).json({ message: 'League not found.' });
-  //     }
-
-  //     // Check if the user is part of the league
-  //     if (!league.userId.includes(userId)) { // Assuming league.userIds is an array of user IDs
-  //       return res.status(403).json({ message: 'User is not a member of this league.' });
-  //     }
-
-  //     // Create a new message
-  //     const newMessage = new Message({
-  //       leagueId,
-  //       userId,
-  //       message,
-  //       status: 1 // Assuming status 1 means 'sent'
-  //     });
-
-  //     // Save the message
-  //     const savedMessage = await newMessage.save();
-
-  //     res.status(201).json({
-  //       message: 'Message sent successfully.',
-  //       data: savedMessage
-  //     });
-  //   } catch (err) {
-  //     console.error(err.message);
-  //     res.status(500).json({ message: err.message });
-  //   }
-  // }
- 
-  
- 
+   
 
   // Show all messages for a league
   static showAllMessages = async (req, res) => {
