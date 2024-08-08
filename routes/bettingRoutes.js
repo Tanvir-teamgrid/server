d8eb15ee99a208bdaa6a0fda2d086cfe7878dbd0
mdtanvirteam <mdtanvir.ahmed@teamgrid.io>
2024-08-08 14:09:13 +0530

    added

--- routes/bettingRoutes.js
+++ routes/bettingRoutes.js
@@ -1,7 +1,8 @@
-const express = require('express');
+
+const express = require("express");
 const router = express.Router();
 const {placeBet,handleMatchEnd,getUserBets} = require('../controllers/bettingController');
- 
+
 
 // Route to place a bet
 router.post('/place-bet',placeBet);
@@ -11,5 +12,5 @@ router.post('/handle-match-end/:matchId',handleMatchEnd);
 
 // Route to get user bets
 router.get('/user-bets',getUserBets);
-
+ 
 module.exports = router;
